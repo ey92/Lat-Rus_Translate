91dbacb9c4b1ef06fbfaf03edcecdf1067a139f2
echyam <echyam@gmail.com>
2018-04-03 17:24:11 -0400

    latin 1st conj pres tense

--- Latin.py
+++ Latin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-vowLow = [u'а', u'е', u'ё', u'и', u'о', u'у', u'ы', u'э', u'ю', u'я']
-vowCap = [u'А', u'Е', u'Ё', u'И', u'О', u'У', u'Ы', u'Э', u'Ю', u'Я']
+vowLow = ['a', 'e', 'i', 'o', 'u']
+vowCap = ['A', 'E', 'I', 'O', 'U']
 vow = vowLow+vowCap
-consLow = [u'б', u'в', u'г', u'д', u'ж', u'з', u'й', u'к', u'л', u'м', u'н', u'п', u'р', u'с', u'т', u'ф', u'х', u'ц', u'ч', u'ш', u'щ', u'ъ', u'ь']
-consCap = [u'Б', u'В', u'Г', u'Д', u'Ж', u'З', u'Й', u'К', u'Л', u'М', u'Н', u'П', u'Р', u'С', u'Т', u'Ф', u'Х', u'Ц', u'Ч', u'Ш', u'Щ', u'Ъ', u'Ь']
+consLow = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
+consCap = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Z']
 cons = consLow+consCap
 lettersLow = vowLow+consLow
 lettersCap = vowCap+consCap
@@ -16,23 +16,18 @@ NUM_CONS_CAP = len(consCap)
 NUM_LETTERS = len(letters)
 NUM_LETTERS_CAP = len(lettersCap)
 
-def takeRaw(word):
-	return word.decode('utf8')
+endings = {"FSTSG":"o","SNDSG":"s","TRDSG":"t","FSTPL":"mus","SNDPL":"tis","TRDPL":"nt"}
 
 def getIndexL(letter):
-	#letter = takeRaw(letter)
 	return letters.index(letter)
 
 def getIndex(word):
-	word = takeRaw(word)
 	lst = []
 	for i in range(len(word)):
 		lst.append(getIndexL(word[i]))
 	return lst
 
-# map(x if consonants.index(letter) > -1 else CONSONANTS[consonants.index(letter)], word)
 def toUpperL(letter):
-	#letter = takeRaw(letter)
 	return letters[(getIndexL(letter)+(NUM_LETTERS/2))%NUM_LETTERS]
 
 def toUpper(word):
@@ -48,5 +43,18 @@ def toLowerL(letter):
 def toLower(word):
 	toUpper(word)
 
-# def morphology(input):
-	#TODO
+def presentTense(inf, t, num, per):
+	# 1st Conjugations
+	if t[0] == ("1"):
+		if (per+num) == "FSTSG":
+			stem = inf[:-3]
+		else:
+			stem = inf[:-2] 	# cutoff "re" from ending (ama)
+		end = endings[per+num]
+		return stem+end
+
+
+# takes infinitive, type, number, person, tense
+def conjugate(inf, t, per, num, tense):
+	if tense == ("PRES"):
+		return presentTense(inf,t,num,per)
